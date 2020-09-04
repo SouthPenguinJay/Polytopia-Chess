67acd2a6b3ebd02a373d9e422ea166c93307289a
Artemis21 <hollyshort21@gmail.com>
2020-09-04 16:40:52 +0100

    Use enums for winner and conclusion type.

--- server/models.py
+++ server/models.py
@@ -1,10 +1,12 @@
 """Various Peewee models."""
+import datetime
+import enum
+import hashlib
+import typing
+
 import config
 
 import peewee as pw
-
-import datetime
-import hashlib
 
 
 HASHING_ALGORITHM = hashlib.sha3_512
@@ -21,10 +23,54 @@ class HashedPassword:
         """Store the hashed password."""
         self.hashed_password = hashed_password
 
-    def __eq__(self, password: str):
+    def __eq__(self, password: str) -> bool:
         """Check for equality against an unhashed password."""
         hashed_attempt = HASHING_ALGORITHM(password.encode()).hexdigest()
         return hashed_attempt == self.hashed_password
+
+
+class Winner(enum.Enum):
+    """An enum for the winner of a game."""
+
+    GAME_NOT_COMPLETE = enum.auto()
+    HOME = enum.auto()
+    AWAY = enum.auto()
+    DRAW = enum.auto()
+
+
+class Conclusion(enum.Enum):
+    """An enum for the way a game finished."""
+
+    GAME_NOT_COMPLETE = enum.auto()
+    CHECKMATE = enum.auto()
+    RESIGN = enum.auto()
+    TIME = enum.auto()
+    STALEMATE = enum.auto()
+    THREEFOLD_REPETITION = enum.auto()
+    FIFTY_MOVE_RULE = enum.auto()
+    AGREED_DRAW = enum.auto()
+
+
+class EnumField(pw.SmallIntegerField):
+    """A field where each value is an integer representing an option."""
+
+    def __init__(
+            self, options: enum.Enum, **kwargs: typing.Dict[str, typing.Any]):
+        """Create a new enum field."""
+        self.options = options
+        super().__init__(**kwargs)
+
+    def python_value(self, raw: typing.Any) -> enum.Enum:
+        """Convert a raw number to an enum value."""
+        number = super().python_value(raw)
+        return self.options(number)
+
+    def db_value(self, instance: enum.Enum) -> typing.Any:
+        """Convert an enum value to a raw number."""
+        if not isinstance(instance, self.options):
+            raise TypeError(f'Instance is not of enum class {self.options}.')
+        number = instance.value
+        return super().db_value(number)
 
 
 class BaseModel(pw.Model):
@@ -51,7 +97,7 @@ class User(BaseModel):
     @property
     def password(self) -> HashedPassword:
         """Return an object that will use hashing in it's equality check."""
-        return HashedPassword(self, password_hash)
+        return HashedPassword(self, self.password_hash)
 
     @password.setter
     def password(self, password: str):
@@ -61,7 +107,7 @@ class User(BaseModel):
 
 class Game(BaseModel):
     """A model to represent a game.
-    
+
     The game may be in any of the following states:
       1. Open
           A player is looking for a game matching these specs, but a second
@@ -82,15 +128,10 @@ class Game(BaseModel):
     home_offering_draw = pw.BooleanField(default=False)
     away_offering_draw = pw.BooleanField(default=False)
     # TODO: Represent the board somehow.
-
-    # 0=not finished, 1=home, 2=away, 3=draw
-    winner = pw.SmallIntegerField(default=0)
-
-    # 0=not finished, 1=checkmate, 2=resign, 3=agreed draw, 4=stalemate
-    # 5=threefold repetition, 6=50 move rule, 7=time
-    # TODO: Are there more possible conclusions?
-    conclusion_type = pw.SmallIntegerField(default=0)
-
+    winner = EnumField(Winner, default=Winner.GAME_NOT_COMPLETE)
+    conclusion_type = EnumField(
+        Conclusion, default=Conclusion.GAME_NOT_COMPLETE
+    )
     opened_at = pw.DateTimeField(default=datetime.datetime.now)
     started_at = pw.DateTimeField(null=True)
     ended_at = pw.DateTimeField(null=True)
